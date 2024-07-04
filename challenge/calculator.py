e62b77303fe0621313a07c6f4f0e7733e0d52d23
DanielMevs <dannymevs@hotmail.com>
2024-07-03 20:11:49 -0400

    completed calculator context challenge

--- challenge/calculator.py
+++ challenge/calculator.py
@@ -1,2 +1,10 @@
 class Calculator:
-    pass
+    def __init__(self, *exc_types):
+        self.exc_types = exc_types
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.error = exc_value
+        return isinstance(exc_value, self.exc_types)
