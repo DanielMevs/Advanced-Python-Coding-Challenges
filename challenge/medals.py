2c87887c6feeb426b6d43e037c811d2973212d8a
DanielMevs <dannymevs@hotmail.com>
2024-07-05 20:01:49 -0400

    completed medals challenge

--- challenge/medals.py
+++ challenge/medals.py
@@ -7,7 +7,22 @@ medal = namedtuple('medal', ['City', 'Edition', 'Sport', 'Discipline', 'Athlete'
        'Event', 'Event_gender', 'Medal'])
 
 medals = [] #Complete this - medals is a list of medal namedtuples
+for entry in olympics.split('\n')[:-1]:
+    entry_list = entry.split(';')
+    new_entry = medal(*entry_list)
+    medals.append(new_entry)
+
 
 def get_medals(**kwargs):
     '''Return a list of medal namedtuples '''
-    pass
+    result = []
+    
+    for entry in medals:
+        count = 0
+        for col, val in kwargs.items():
+            if getattr(entry, col) == val:
+                count += 1
+        if count == len(kwargs):
+            result.append(entry)
+    
+    return result
